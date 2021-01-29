c89f023d23ed083f050981a2edb0a0ac41df1400
Ana Carolina Arellano <pg20@vfs.com>
2021-01-29 16:45:16 -0600

    Added start of game

---
+++ scripts/Game.js
@@ -0,0 +1,48 @@
+'use strict';
+
+export default class Game{
+
+    constructor(){
+        this.board = {
+            size : 15
+        };
+        this.gameOver = false;
+    }
+    run(){
+        while(!this.gameOver){
+            this.update();
+            this.render();
+        }
+    }
+
+    update() {
+        //get user input and update the game simulation
+        this.gameOver = true;
+    }
+
+    render() {
+        //change the DOM and the screen to show the player what's going on
+        
+        //generate the playfield
+        this.generateBoard();
+    }
+
+    generateBoard(){
+        /*
+        <table>
+            <tr><td></td>...</tr>
+        </table>
+        */ 
+       let markup = "<table>";
+       for(let row = 0; row < this.board.size; row++){
+           markup += "<tr>";
+           for(let col = 0; col < this.board.size; col++){
+               markup += "<td></td>";
+           }
+           markup += "</tr>";
+       }
+       markup += "</table>";
+
+       document.querySelector("#game-screen").innerHTML = markup;
+    }
+}
---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+//Copyright (c) 2021,, Ana Carolina Arellano
+'use strict';
+
+import Game from './Game.js';
+//Main appplication
+let game = new Game();
+game.run();
